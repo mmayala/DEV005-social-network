811b51bd08c05d42943491cd4b2d0c6a91c24019
LauraCMonteroM <lauramontero205@gmail.com>
2023-04-10 12:27:29 -0500

    agrega algunas modificaciones a imágenes, estilos

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,23 +1,26 @@
-/* import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword } from "firebase/auth";
 //import { async } from 'regenerator-runtime';
-import { auth } from './firebase.js'; 
+import { auth } from "./firebase.js";
 
 export function signin() {
-  const signinForm = document.querySelector('#signin_form');
+  const signinForm = document.querySelector("#signin_form");
   // console.log(signinForm);
 
-  signinForm.addEventListener('submit', async (e) => {
+  signinForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const email = signinForm.signin_email.value;
     const password = signinForm.signin_password.value;
     console.log(email, password);
 
     try {
-      const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       console.log(userCredentials);
     } catch (error) {
       console.log(error);
     }
   });
 }
- */
--- src/main.js
+++ src/main.js
@@ -1,34 +1,38 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
-import { app, analytics, auth} from './firebase.js';
+import { app, analytics, auth } from './firebase.js';
 import login from './components/login.js';
 //import { signin } from './components/signin.js';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 root.append(login());
 
-
 function signin() {
-    const signinForm = document.querySelector('#signin_form');
-    // console.log(signinForm);
-  
-    signinForm.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const email = signinForm.signin_email.value;
-      const password = signinForm.signin_password.value;
-      console.log(email, password);
-  
-      try {
-        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-        console.log(userCredentials);
-      } catch (error) {
-        console.log(error);
-      }
-    });}
+  const signinForm = document.querySelector('#signin_form');
+  // console.log(signinForm);
 
-    signin();
+  signinForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const email = signinForm.signin_email.value;
+    const password = signinForm.signin_password.value;
+    console.log(email, password);
+
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      console.log(userCredentials);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
+signin();
 
 myFunction();
 
