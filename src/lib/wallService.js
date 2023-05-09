cc0181d22c2475a350b8369a0f82813341edd109
LauraCMonteroM <lauramontero205@gmail.com>
2023-05-09 17:29:37 -0500

    agrega pequeños camibios

--- src/lib/wallService.js
+++ src/lib/wallService.js
@@ -1,10 +1,10 @@
-import wall from "../components/wall";
+// import wall from "../components/wall";
 
 // Evento que captura dato de text area
-export const createPost = divWall
+/* export const createPost = divWall
   .querySelector('#buttonPost')
   .addEventListener('click', () => {
     const comment = divWall.querySelector('#commentPost');
     addPost(comment.value);
     //comment.value = ''
-  });
+  }); */
